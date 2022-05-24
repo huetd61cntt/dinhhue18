b482222a69ff725d45e24547c5e86b57a333ec32
huetd61cntt <100772468+huetd61cntt@users.noreply.github.com>
2022-05-24 21:10:39 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,62 +1,45 @@
-package tinhchuvi.hue61133694;
+package cau2.thd61133694;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    EditText chieudai, chieurong;
-    Button tinhChuvi, tinhDientich;
-    TextView ketqua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        chieudai = (EditText)findViewById(R.id.chieudai);
-        chieurong = (EditText)findViewById(R.id.chieurong);
-        tinhChuvi = (Button)findViewById(R.id.tinhChuvi);
-        tinhDientich = (Button)findViewById(R.id.tinhDientich);
-        ketqua = (TextView)findViewById(R.id.ketqua);
-
-        tinhChuvi.setOnClickListener(new View.OnClickListener() {
+        ArrayList<String> listSong =  new ArrayList<String>();
+        listSong.add("To_Huu");
+        listSong.add("Tu_ay");
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listSong);
+        listSong.add("To_Huu");
+        listSong.add("Bac_oi");
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_2,listSong);
+        ListView lv= (ListView)findViewById(R.id.listView_Cac_cai_tho);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                int cdai = Integer.parseInt(chieudai.getText().toString());
-                int crong = Integer.parseInt(chieurong.getText().toString());
-                int kqua = (cdai+crong)*2;
-                ketqua.setText("chu vi hinh chu nhat la: " + kqua);
-            }
-        });
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        tinhDientich.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int cdai = Integer.parseInt(chieudai.getText().toString());
-                int crong = Integer.parseInt(chieurong.getText().toString());
-                int kqua = cdai*crong;
-                ketqua.setText("dien tich hinh chu nhat la: " + kqua);
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(View, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String tenBaiTho = listSong.get(i);
+                // Chuyển Activity
+                Intent intentBH = new Intent(MainActivity.this, ShowLyricActivity.class);
+                // gói tên vào
+                intentBT.putExtra("tenBT",tenBaiTho);
+                // Đẩy sang
+                startActivity(intentBT);
             }
         });
     }
+
+
 }
